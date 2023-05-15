7a02f3ae1ee8aadfb6c59f302af8b596f297e10e
wrsuperboy <wwr42@126.com>
2023-05-15 18:59:38 +0800

    Update configs

--- Source/PAL.Target.cs
+++ Source/PAL.Target.cs
@@ -9,6 +9,7 @@ public class PALTarget : TargetRules
 	{
 		Type = TargetType.Game;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.AddRange( new string[] { "PAL" } );
 	}
 }
